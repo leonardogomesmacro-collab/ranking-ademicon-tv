3165602dcd7349e166fe5bc3bd9e41fd0c63b35d
leonardogomesmacro-collab <leonardogomes.macro@gmail.com>
2026-06-15 20:09:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSS25FN1X-8TacEC-uufmvExEJalfrykTVr0lTS6UUhqzkM_kfSKBufhvx-reF6J92gJY06sLtBIYyP8DfeYyhhuA0kHIGIVkxcABxV_z1XU9W40PJmG9Jpz7ypArvB472GfiyT8ghM2Ubff9k3YmqwK0tWCAwU0UA1GNKoD8shozrP5p0pJR6XeeFDekiiZXRy8bNp6DN4OEpxjG9c8My1J18QjkHters3UdkgCjUqecgulf5ysEXQuZAHI_ZSjSbZKRS9vyiTBXboymNBw2Dks6yyuw&lib=MgTdPYGUPrZnH6zMWb77KTExsl7XT8uTg";
+const API = "https://script.google.com/macros/s/AKfycbzKzm7xjNdPLjCfKLro39FCy6S1f9UEmR-t2sXFPdlo4GWysUawT1VtF7ahQfjUytVl/exec";
 
 async function carregarRanking() {
 
