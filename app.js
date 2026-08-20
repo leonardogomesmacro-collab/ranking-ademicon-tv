b454b561d0031b3db2ef1e00856db087937c3c23
leonardogomesmacro-collab <leonardogomes.macro@gmail.com>
2026-08-20 15:21:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = https://script.google.com/macros/s/AKfycbwhrz4NYBkZhO11hxauYPyiEtAHMprAwh5yLff4Jx3rP4Fc5HRZ8X7suAtH-SMwVPvj_w/exec;
+const API_URL = "https://script.google.com/macros/s/AKfycbwhrz4NYBkZhO11hxauYPyiEtAHMprAwh5yLff4Jx3rP4Fc5HRZ8X7suAtH-SMwVPvj_w/exec";
 
 console.log("APP.JS CARREGADO");
 
